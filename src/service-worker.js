88ec284aa85eebf90c2ca1798502fc8279969234
Muhammad Yusrizal <rizal.adoy@gmail.com>
2022-08-25 09:46:50 +0700

    service worker: cache fonts

--- src/service-worker.js
+++ src/service-worker.js
@@ -11,7 +11,7 @@ import { clientsClaim } from 'workbox-core';
 import { ExpirationPlugin } from 'workbox-expiration';
 import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
-import { StaleWhileRevalidate } from 'workbox-strategies';
+import { NetworkFirst, StaleWhileRevalidate } from 'workbox-strategies';
 
 clientsClaim();
 
@@ -81,6 +81,7 @@ self.addEventListener('install', (event) => {
 
   event.waitUntil(asyncInstall);
 });
+
 self.addEventListener('activate', (event) => {
   console.log('Activate !: ', event);
 
@@ -97,3 +98,18 @@ self.addEventListener('activate', (event) => {
 
   event.waitUntil(asyncActivate);
 });
+
+// Cache font CDN
+registerRoute(
+  ({ url }) =>
+    url.origin === 'https:fonts.googleapis.com' || url.origin === 'https://fonts.gstatic.com',
+  new NetworkFirst({
+    cacheName: 'fonts',
+    plugins: [
+      new ExpirationPlugin({
+        maxAgeSeconds: 60 * 60 * 24 * 356,
+        maxEntries: 30
+      })
+    ]
+  })
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.{js,jsx}'],
-    theme: {
-        extend: {},
+  content: ['./src/**/*.{js,jsx}'],
+  theme: {
+    fontFamily: {
+      sans: ['"Josefin Sans"'],
+      serif: ['ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+      mono: [
+        'ui-monospace',
+        'SFMono-Regular',
+        'Menlo',
+        'Monaco',
+        'Consolas',
+        '"Liberation Mono"',
+        '"Courier New"',
+        'monospace'
+      ]
     },
-    plugins: [],
-}
+    extend: {}
+  },
+  plugins: []
+};
